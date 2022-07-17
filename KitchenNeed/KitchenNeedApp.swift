29c0068cd51190866c8fb1cf5f87e430543db3da
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2022-07-17 21:43:46 +0100

    Initial Commit

---
+++ KitchenNeed/KitchenNeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenNeedApp.swift
+//  KitchenNeed
+//
+//  Created by Bouchedoub Rmazi on 17/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct KitchenNeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
