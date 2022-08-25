fb85b12d054f30504412997e11bc73dd0afef1ff
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2022-08-25 11:33:58 +0100

    feata: create prospect

---
+++ KitchenNeed/Models/Prospect.swift
@@ -0,0 +1,24 @@
+//
+//  Prospect.swift
+//  KitchenNeed
+//
+//  Created by Bouchedoub Rmazi on 25/8/2022.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = ""
+    var details = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+
+    init() {
+        self.people = []
+    }
+}
+
