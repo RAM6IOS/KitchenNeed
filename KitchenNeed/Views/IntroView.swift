726a98bc65fd7434ae06c4441902b4781b386c8d
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2022-09-02 18:53:21 +0100

    Refactor: change onboarding view

---
+++ KitchenNeed/Views/IntroView.swift
@@ -0,0 +1,20 @@
+//
+//  IntroView.swift
+//  KitchenNeed
+//
+//  Created by Bouchedoub Rmazi on 2/9/2022.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroView()
+    }
+}
--- KitchenNeed/Views/Onboarding.swift
+++ KitchenNeed/Views/Onboarding.swift
@@ -6,15 +6,57 @@
 //
 
 import SwiftUI
-
+import CoreMIDI
 struct Onboarding: View {
     @Binding  var ShowOnboarding :Bool
+    @State var currentPage = 0
     var body: some View {
-        TabView{
-            SignupView(ShowOnboarding: $ShowOnboarding)
-        
+        GeometryReader(content: { geometry in
+        VStack{
+        TabView(selection: $currentPage){
+            IntroView()
+                .tag(0)
+            IntroView()
+                .tag(1)
+    SignupView(ShowOnboarding: $ShowOnboarding)
+                .tag(2)
         }
           .tabViewStyle(PageTabViewStyle())
+          .indexViewStyle(.page(backgroundDisplayMode: .always))
+            HStack{
+                if currentPage > 0 {
+                    Button{
+                        currentPage -= 1
+                    } label: {
+                        Text("back")
+                            .foregroundColor(.white)
+                            .frame(width: 150, height: 50)
+                            .background(Color.green)
+                            .cornerRadius(10)
+                    }
+                    
+                }
+                if   currentPage <= 1{
+                Button{
+                    if currentPage != 2 {
+                        currentPage += 1
+                    }
+                } label: {
+                    HStack{
+                        Text( "Next")
+                            
+                     Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 150, height: 50)
+                    .background(Color.green)
+                    .cornerRadius(10)
+                }
+                }
+            }
+            Spacer()
+    }
+        })
     }
 }
 
