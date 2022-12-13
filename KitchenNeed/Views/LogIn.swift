979010b45bde0fa6c01b14ae2b22c624aa7a542e
Bouchedoub Ramzi <ios191169@gmail.com>
2022-12-13 18:51:47 +0100

    Cut : Products Card

--- KitchenNeed/Views/LogIn.swift
+++ KitchenNeed/Views/LogIn.swift
@@ -28,7 +28,6 @@ struct LogIn: View {
             }
             Button{
                 withAnimation{
-                  // ShowHome.toggle()
                     viewModel.login(withEmail: Email, password: password)
                 }
                print("reds")
