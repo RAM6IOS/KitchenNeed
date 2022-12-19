c6aea5d935414ffed31414b5bf6c8ff0bd78bd82
Bouchedoub Ramzi <ios191169@gmail.com>
2022-12-19 10:25:24 +0100

    Refactor : UI card product

--- KitchenNeed/Views/CardView.swift
+++ KitchenNeed/Views/CardView.swift
@@ -18,14 +18,47 @@ struct CardView: View {
     }
     
     var body: some View {
-        List{
-            ForEach(viewModel.product){ product  in
-                Text(product.name)
-
+        ScrollView{
+            LazyVStack(alignment: .leading){
+                ForEach(viewModel.product){ product  in
+                    HStack(alignment: .center) {
+                        Image(product.type)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 80)
+                                        .padding(.all, 20)
+                        
+                        VStack(alignment: .leading) {
+                            Text(product.type)
+                                            .font(.system(size: 20, weight: .bold, design: .default))
+                                            .foregroundColor(.black)
+                            Text(product.name)
+                                            .font(.system(size: 16, weight: .bold, design: .default))
+                                            .foregroundColor(.gray)
+                            
+                            Text(product.name)
+                                            .font(.system(size: 16, weight: .bold, design: .default))
+                                            .foregroundColor(.gray)
+                                        
+                                    }.padding(.trailing, 20)
+                        
+                                    Spacer()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    //.background(Color(red: 32/255, green: 36/255, blue: 38/255))
+                    .background(.white)
+                    .modifier(CardModifier())
+                    .padding(.all, 10)
+                    
+                    
+                    
+                }
             }
+            
         }
             .foregroundColor(Color(.label))
             .contentShape(Rectangle())
+            
             .onLongPressGesture(minimumDuration:0.2, maximumDistance:10 ){
                 isPresented2.toggle()
             }
@@ -39,3 +72,11 @@ struct CardView: View {
     }
     
 
+struct CardModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .cornerRadius(20)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 0)
+    }
+    
+}
--- KitchenNeed/Views/SheetView.swift
+++ KitchenNeed/Views/SheetView.swift
@@ -13,67 +13,60 @@ struct SheetView: View {
     @State private var  details = ""
     let types = ["vegetable", "meat", "fruits", "bread" , "milk" ,"spices" ,"canned-food" ,"cleaning-materials"]
     @State private var type = "vegetable"
-    
     @Environment(\.managedObjectContext) var moc
     @EnvironmentObject var prospects: Prospects
     @ObservedObject var viewModel = UploadProductViewModel()
     var body: some View {
         NavigationView{
-            VStack{
-                Form{
-                    Section("Types"){
-                Picker("types", selection: $type) {
+           
+                VStack{
+                    Form{
+                        Section("Types"){
+                            Picker("types", selection: $type) {
                                 ForEach(types, id: \.self) { typ in
                                     HStack( spacing: 10){
-                                    Text(typ)
+                                        Text(typ)
                                     }
                                 }
                             }
-                        Image(type)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 80, height: 80)
-                            .cornerRadius(12)
-                        
+                            Image(type)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(12)
+                            
+                        }
+                        Section{
+                            TextField("Name" , text: $name)
+                        }
+                        Section{
+                            TextField("price" , text:$quantity)
+                        }
                     }
-                    Section{
-                TextField("Name" , text: $name)
-                    }
-                    Section{
-                TextField("price" , text:$quantity)
-                    }
-                }
-                     
-                Button{
-                    /*
-                    let newProduct = Product(context: moc)
-                    newProduct.type = type
-                    newProduct.name = name
-                    newProduct.quantity = quantity
-                    newProduct.details = details
-                    try? moc.save()
-                     */
-                    //let prospect = Prospect()
-                   // prospect.name = name
-                   // prospects.people.append(prospect)
-                    viewModel.uploadProduct(withCaption: name, quantity: quantity, type: type)
-                    quantity = ""
-                    name = ""
-                    type = ""
+                    .background(Color(red: 32/255, green: 36/255, blue: 38/255))
                     
-                } label: {
-                    Text("Save")
-                        .bold()
-                        .font(.title3)
-                        .frame(width: 250, height: 40)
-                        .foregroundColor(.white)
+                    Button{
+                        viewModel.uploadProduct(withCaption: name, quantity: quantity, type: type)
+                        quantity = ""
+                        name = ""
+                        type = ""
+                        
+                    } label: {
+                        Text("Save")
+                            .bold()
+                            .font(.title3)
+                            .frame(width: 250, height: 40)
+                            .foregroundColor(.white)
+                    }
+                    .background(Color.green)
+                    .cornerRadius(10)
                 }
-                .background(Color.green)
-                .cornerRadius(10)
-            }
-            .navigationTitle("Add Product")
+                .navigationTitle("Add Product")
+                
+                
             
         }
+       
 
         
     }
