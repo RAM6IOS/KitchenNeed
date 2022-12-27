83bc084270c53100cb1a7bb831e850fc2547fda0
Bouchedoub Ramzi <ios191169@gmail.com>
2022-12-27 19:20:36 +0100

    add: recipe details view

---
+++ KitchenNeed/Views/RecipeSetails.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeSetails.swift
+//  KitchenNeed
+//
+//  Created by Bouchedoub Ramzi on 27/12/2022.
+//
+
+import SwiftUI
+
+struct RecipeSetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeSetails_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeSetails()
+    }
+}
