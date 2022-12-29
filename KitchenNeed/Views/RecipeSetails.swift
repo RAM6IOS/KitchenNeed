ca08b3f92857db43b48dbaf7e17ad37680a55e68
Bouchedoub Ramzi <ios191169@gmail.com>
2022-12-29 19:37:32 +0100

    add: ui recipe details view

--- KitchenNeed/Views/RecipeSetails.swift
+++ KitchenNeed/Views/RecipeSetails.swift
@@ -6,15 +6,59 @@
 //
 
 import SwiftUI
+import Kingfisher
 
-struct RecipeSetails: View {
+struct RecipeDetailsView: View {
+    let recipe: Recipet
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Display the name of the recipe at the top
+            Text(recipe.name)
+                .font(.largeTitle)
+                .padding()
+
+            // Display the recipe image
+            KFImage(URL(string: recipe.recipetImageUrl))
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: nil, idealWidth: nil, maxWidth: UIScreen.main.bounds.width, minHeight: nil, idealHeight: nil, maxHeight: 300, alignment: .center)
+                .clipped()
+            
+            Text(recipe.name)
+                .font(.largeTitle)
+                .padding()
+
+            // Display the preparation time and temperature
+            HStack {
+                Text("Preparation time: \(recipe.time) minutes")
+                    .font(.headline)
+                Spacer()
+                Text("Cooking temperature: \(recipe.degree)°C")
+                    .font(.headline)
+            }
+            .padding()
+
+            // Display the ingredients and instructions in a list
+            List {
+                Section(header: Text("Ingredients")) {
+                    
+                        Text(recipe.ingredients)
+                            .font(.largeTitle)
+                            .padding()
+                    
+                }
+
+                Section(header: Text("Instructions")) {
+                    
+                        Text(recipe.definition)
+                            .font(.largeTitle)
+                            .padding()
+                    
+                }
+            }
+        }
     }
 }
 
-struct RecipeSetails_Previews: PreviewProvider {
-    static var previews: some View {
-        RecipeSetails()
-    }
-}
+
