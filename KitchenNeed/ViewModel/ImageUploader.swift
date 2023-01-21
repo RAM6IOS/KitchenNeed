73eb8ea39f04e4b08e9f9d4f0f8e2230c2d81894
Bouchedoub Ramzi <ios191169@gmail.com>
2023-01-21 16:57:22 +0100

    Cut: remove file profile view model

--- KitchenNeed/ViewModel/ImageUploader.swift
+++ KitchenNeed/ViewModel/ImageUploader.swift
@@ -3,28 +3,21 @@
 //  KitchenNeed
 //
 //  Created by Bouchedoub Ramzi on 13/12/2022.
-//
-
 import Foundation
 import Firebase
 import FirebaseStorage
 import UIKit
 
-
 struct ImageUploader {
-    
     static func uploadImage(image: UIImage, completion: @escaping(String) -> Void) {
         guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
-        
         let filename = NSUUID().uuidString
         let ref = Storage.storage().reference(withPath: "/profile_image/\(filename)")
-        
         ref.putData(imageData, metadata: nil) { _, error in
             if let error = error {
                 print("DEBUG: Failed to upload image with error: \(error.localizedDescription)")
                 return
             }
-            
             ref.downloadURL { imageUrl, _ in
                 guard let imageUrl = imageUrl?.absoluteString else { return }
                 completion(imageUrl)
@@ -33,16 +26,13 @@ struct ImageUploader {
     }
     static func uploadImage2(image: UIImage, completion: @escaping(String) -> Void) {
         guard let imageData = image.jpegData(compressionQuality: 0.5) else { return }
-        
         let filename = NSUUID().uuidString
         let ref = Storage.storage().reference(withPath: "/recipe_image/\(filename)")
-        
         ref.putData(imageData, metadata: nil) { _, error in
             if let error = error {
                 print("DEBUG: Failed to upload image with error: \(error.localizedDescription)")
                 return
             }
-            
             ref.downloadURL { imageUrl, _ in
                 guard let imageUrl = imageUrl?.absoluteString else { return }
                 completion(imageUrl)
