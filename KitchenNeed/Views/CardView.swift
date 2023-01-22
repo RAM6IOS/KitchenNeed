794d4a8ad17caf578b35d3edf38f93c10e9755a5
Bouchedoub Ramzi <ios191169@gmail.com>
2023-01-22 12:26:53 +0100

    add : card modifier

--- KitchenNeed/Views/CardView.swift
+++ KitchenNeed/Views/CardView.swift
@@ -5,9 +5,7 @@
 import SwiftUI
 import Firebase
 struct CardView: View {
-    @State private var isPresented = false
-    @State private var isPresented2 = false
-    @ObservedObject var viewModel :  ProductViewModel
+    @ObservedObject var viewModel : ProductViewModel
     init(user:User){
         self.viewModel = ProductViewModel(user: user)
     }
@@ -46,10 +44,8 @@ struct CardView: View {
                                     .font(.system(size: 25, weight: .bold, design: .default))
                                     .foregroundColor(.black)
                                 .padding(.top , -10)
-                                
                             }
                         }
-                        
                         Spacer()
                     }
                 }
@@ -57,7 +53,6 @@ struct CardView: View {
                 .overlay(
                     Button {
                         guard let producId = product.id else { return }
-                        // [START delete_document]
                         Firestore.firestore().collection("product").document(producId).delete() { err in
                             if let err = err {
                                 print("Error removing document: \(err)")
@@ -76,41 +71,9 @@ struct CardView: View {
                 .modifier(CardModifier())
                 .foregroundColor(Color(.label))
                 .contentShape(Rectangle())
-                /*.alert("do you want to delete? ", isPresented: $isPresented2, actions: {
-                    Button("No", role: .cancel, action: {})
-                    Button("Delete", role: .destructive, action: {
-                        //guard let uid = Auth.auth().currentUser?.uid else { return }
-                        guard let producId = product.id else { return }
-                        // [START delete_document]
-                        Firestore.firestore().collection("product").document(producId).delete() { err in
-                            if let err = err {
-                                print("Error removing document: \(err)")
-                            } else {
-                                print("Document successfully removed!")
-                            }
-                        }
-                        Firestore.firestore().collection("product").document(producId).delete { error in
-                            if let error = error {
-                                print("Unable to remove card: \(error.localizedDescription)")
-                            }
-                        }
-                    })
-                }, message: {
-                    Text("Message")
-                })*/
                 .padding(.all, 10)
             }
         }
-        
     }
 }
 
-
-struct CardModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .cornerRadius(20)
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 0)
-    }
-    
-}
