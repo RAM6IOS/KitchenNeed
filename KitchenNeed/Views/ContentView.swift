27984840f622cfbdc8406e47aaeaf9b0fce59f99
Bouchedoub Ramzi <ios191169@gmail.com>
2023-01-22 11:38:05 +0100

    Refactor : content view

--- KitchenNeed/Views/ContentView.swift
+++ KitchenNeed/Views/ContentView.swift
@@ -1,57 +1,41 @@
 //
 //  ContentView.swift
 //  KitchenNeed
-//
-//  Created by Bouchedoub Rmazi on 17/7/2022.
-
 import SwiftUI
 struct ContentView: View {
     @State var ShowOnboarding = true
     @State var ShowHome = true
     @EnvironmentObject var viewModel: AuthViewModel
-   // let recipet = viewModel.recipe
     var body: some View {
         TabView {
             if viewModel.userSession == nil {
                 SignupView( ShowHome: $ShowHome)
-               
             } else{
                 if let user = viewModel.currentUser {
                 Home()
                     .tabItem {
-                        
                         Label("List", systemImage: "list.bullet.rectangle.portrait.fill")
                     }
                 Recipe()
                     .tabItem {
                         Label("Group", systemImage: "person.3")
                     }
-               
-                    
                         Profile(user: user)
                             .tabItem {
                                 Label("Profile", systemImage: "person.fill")
                             }
-                    
-                
             }
             }
         }
         .onAppear {
-           
                    if #available(iOS 15.0, *) {
                            let appearance = UITabBarAppearance()
                            UITabBar.appearance().scrollEdgeAppearance = appearance
                        }
                            }
-
         .fullScreenCover(isPresented:$ShowOnboarding, content: {
             Onboarding(ShowOnboarding: $ShowOnboarding)
         })
     }
 }
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
